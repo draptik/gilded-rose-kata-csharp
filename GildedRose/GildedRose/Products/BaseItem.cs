db5b6d8190a6bd910c3fc81957c03d5a1d585c6c
Patrick Drechsler <patrick@pdrechsler.de>
2019-11-15 19:01:53 +0100

    gilded rose: OO solution - add null check

--- GildedRose/GildedRose/Products/BaseItem.cs
+++ GildedRose/GildedRose/Products/BaseItem.cs
@@ -1,10 +1,12 @@
+using System;
+
 namespace GildedRose.Products
 {
     public abstract class BaseItem
     {
         protected BaseItem(Item item)
         {
-            Item = item;
+            Item = item ?? throw new Exception("Item must not be null!");
         }
 
         protected Item Item { get; }
