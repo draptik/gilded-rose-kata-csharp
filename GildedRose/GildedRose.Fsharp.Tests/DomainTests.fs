70745a74e482614561871dc5876583dd43375277
Patrick Drechsler <patrick@pdrechsler.de>
2019-11-21 18:30:15 +0100

    gilded rose: FSharp - reformat tests - add happy-case test for AgeByOneDay

--- GildedRose/GildedRose.Fsharp.Tests/DomainTests.fs
+++ GildedRose/GildedRose.Fsharp.Tests/DomainTests.fs
@@ -1,13 +1,9 @@
 module GildedRose.Fsharp.DomainTests
 
-open GildedRose.Fsharp
-
 open FsUnit
 open GildedRose.Fsharp.Domain
 open Xunit
 open Swensen.Unquote
-
-open Domain
 
 [<Theory>]
 [<InlineData(1, 0)>]
@@ -20,11 +16,12 @@ open Domain
 [<InlineData(50, 49)>]
 [<InlineData(49, 48)>]
 let ``decreasing Quality including edge cases`` (input, expected) =
-    let expected = Quality expected
     let actual =
         Quality input
         |> decreaseQualityByOne
     
+    let expected = Quality expected
+
     test <@ actual = expected @>
 
 [<Theory>]
@@ -33,9 +30,32 @@ let ``decreasing Quality including edge cases`` (input, expected) =
 [<InlineData(0, -1)>]
 [<InlineData(-99, -100)>]
 let ``decreasing SellIn by one day`` (input, expected) =
-    let expected = SellIn expected
     let actual =
         SellIn input
         |> decreaseSellInByOneDay
     
+    let expected = SellIn expected
+
+    test <@ actual = expected @>
+
+[<Fact>]
+let ``aging a product by 1 day works`` () =
+    let input =
+        {
+            Name = Name "foo"
+            SellIn = SellIn 10
+            Quality = Quality 5
+        }
+        
+    let actual =
+        input
+        |> ageByOneDay
+    
+    let expected =
+        {
+            Name = Name "foo"
+            SellIn = SellIn 9
+            Quality = Quality 4
+        }
+        
     test <@ actual = expected @>
