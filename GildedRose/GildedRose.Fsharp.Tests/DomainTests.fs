bd9c8c3c1289ffe2e40544f20c970706cffd32e1
Patrick Drechsler <patrick@pdrechsler.de>
2019-11-21 18:22:34 +0100

    gilded rose: FSharp more tests

--- GildedRose/GildedRose.Fsharp.Tests/DomainTests.fs
+++ GildedRose/GildedRose.Fsharp.Tests/DomainTests.fs
@@ -7,11 +7,35 @@ open GildedRose.Fsharp.Domain
 open Xunit
 open Swensen.Unquote
 
-[<Fact>]
-let ``decreaseQualityByOne`` () =
-    let input = Quality 1
-    let expected = Quality 0
-    let actual = Domain.decreaseQualityByOne input
+open Domain
+
+[<Theory>]
+[<InlineData(1, 0)>]
+[<InlineData(2, 1)>]
+[<InlineData(3, 2)>]
+[<InlineData(0, 0)>]
+[<InlineData(-1, 0)>]
+[<InlineData(100, 50)>]
+[<InlineData(51, 50)>]
+[<InlineData(50, 49)>]
+[<InlineData(49, 48)>]
+let ``decreasing Quality including edge cases`` (input, expected) =
+    let expected = Quality expected
+    let actual =
+        Quality input
+        |> decreaseQualityByOne
+    
     test <@ actual = expected @>
 
-
+[<Theory>]
+[<InlineData(1, 0)>]
+[<InlineData(100, 99)>]
+[<InlineData(0, -1)>]
+[<InlineData(-99, -100)>]
+let ``decreasing SellIn by one day`` (input, expected) =
+    let expected = SellIn expected
+    let actual =
+        SellIn input
+        |> decreaseSellInByOneDay
+    
+    test <@ actual = expected @>
